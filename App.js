a3b1e81c4539b45b635485927fb10b2b4835b766
TG9124 <125907014+TG9124@users.noreply.github.com>
2025-03-23 11:08:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './home';
+import './App.css';
+
+function App () {
+    return (
+        <div>
+            <Home />
+        </div>
+        
+    )
+}
+
+export default App;
